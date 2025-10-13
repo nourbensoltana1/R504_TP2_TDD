e7e9550ef99e65ced79d12a1feebb55aaf572e36
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-13 14:00:00 +0200

    Test 4 FAIL – multiples de 5 non remplacés par Buzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,7 +6,7 @@ class TestFizzBuzz(FizzBuzz,unittest.TestCase):
     def test_affiche_exists(self):
         """Vérifie que la méthode affiche() existe et renvoie quelque chose"""
         result = self.affiche()  # appel de la méthode
-        self.assertIn("Fizz", result)
-        self.assertIn("3", result)
+        self.assertIn("Buzz", result)
+        self.assertIn("5", result)
 if __name__ == "__main__":
     unittest.main()
