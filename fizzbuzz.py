0d92928203f04ac589ab957fd9c4f33b2565fa2b
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-13 14:42:08 +0200

    Code 5 SUCCESS – correction de la priorité, multiples de 15 remplacés par FrisBee

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,12 +3,12 @@ class FizzBuzz:
         res=""
         for i in range(1, 101):
             for i in range(1, 101):
-                if i % 3 == 0:
-                    res += "Fizz"
+                if i % 15 == 0:
+                    res += "FrisBee"
                 elif i % 5 == 0:
                     res += "Buzz"
                 elif i % 15 == 0:
-                    res += "FrisBee"
+                    res += "Fizz"
                 else:
                     res += str(i)
         return res
