fb9a199b0eb35b9cab2918ad996cd96a3691169d
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-13 13:57:31 +0200

    Code 3 SUCCESS – remplacement Fizz pour les multiples de 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,9 @@ class FizzBuzz:
     def affiche(self):
         res=""
         for i in range(1, 101):
-            res += str(i)
+            for i in range(1, 101):
+                if i % 3 == 0:
+                    res += "Fizz"
+                else:
+                    res += str(i)
         return res
