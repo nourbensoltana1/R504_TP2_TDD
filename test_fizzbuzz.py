ed3fb6b52c12b2fb2177d0cb017e356ce7f5b44d
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-13 13:39:20 +0200

    Test 2 FAIL – affiche() ne contient pas encore les nombres

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,11 +1,13 @@
 import unittest
-from fizzbuzz import affiche
+from fizzbuzz import FizzBuzz
 
-class TestFizzBuzz(unittest.TestCase):
+class TestFizzBuzz(FizzBuzz,unittest.TestCase):
+
     def test_affiche_exists(self):
-        """Vérifie que la fonction affiche() existe et renvoie quelque chose"""
-        result = affiche()
-        self.assertIsNotNone(result)
-
+        """Vérifie que la méthode affiche() existe et renvoie quelque chose"""
+        result = self.affiche()  # appel de la méthode
+        self.assertIn("1", result)
+        self.assertIn("2", result)
+        self.assertIn("100", result)
 if __name__ == "__main__":
     unittest.main()
