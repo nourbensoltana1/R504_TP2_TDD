bdbd1715378dfb8a590fc4ce38631d957f0575e9
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-13 13:48:17 +0200

    Test 3 FAIL – multiples de 3 non remplacés par Fizz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,8 +6,7 @@ class TestFizzBuzz(FizzBuzz,unittest.TestCase):
     def test_affiche_exists(self):
         """Vérifie que la méthode affiche() existe et renvoie quelque chose"""
         result = self.affiche()  # appel de la méthode
-        self.assertIn("1", result)
-        self.assertIn("2", result)
-        self.assertIn("100", result)
+        self.assertIn("Fizz", result)
+        self.assertIn("3", result)
 if __name__ == "__main__":
     unittest.main()
