8da610ffc902d933d73829f58a6dc9cb6aad6d2c
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-14 11:30:20 +0200

    Test 3 Fail-  la classe crypt ne prend pas en compte le retour à a après un espace

--- Decrypte/test_crypt.py
+++ Decrypte/test_crypt.py
@@ -4,10 +4,10 @@ from crypt import crypt
 class TestCrypt(unittest.TestCase):
     def test_crypt(self):
         "Vérifie que la fonction crypt(message) existe et renvoie quelque chose"
-        message = "abc"
+        message = "ab "
         result = crypt(message)
         print(result)
-        self.assertEqual(result,'bcd')
+        self.assertEqual(result,'bca')
 
 if __name__ == "__main__":
     unittest.main()
