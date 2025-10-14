fb900b15f7e66f82ee82cf0c165f9836cab73aed
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-14 11:35:19 +0200

    Code 3 SUCCEES - Correction du Out of range

--- Decrypte/crypt.py
+++ Decrypte/crypt.py
@@ -4,5 +4,8 @@ def crypt(message):
     result=""
     for i in message:
         position=caracteres.find(i)
-        result+=caracteres[position+1]
+        if position!=len(caracteres)-1:
+            result+=caracteres[position+1]
+        else:
+            result+='a'
     return(result)
