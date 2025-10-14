11445f112594206b0607bbfc8a1bc8eaeed4683f
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-14 09:14:07 +0200

    Test 7 FAIL – affiche(n1, n2) non encore géré

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,7 +5,7 @@ class TestFizzBuzz(FizzBuzz,unittest.TestCase):
 
     def test_affiche_exists(self):
         """Vérifie que la méthode affiche() existe et renvoie quelque chose"""
-        result = self.affiche(15)  # appel de la méthode
+        result = self.affiche(15,20)  # appel de la méthode
         print(result)
 if __name__ == "__main__":
     unittest.main()
