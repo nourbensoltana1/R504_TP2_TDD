1bb5eac844c2435eb051706ea9d6713a3dee6bd8
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-14 09:10:57 +0200

    Code 6 SUCCESS – ajout du paramètre n à la méthode affiche(n) Amelioration du code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,13 @@
 class FizzBuzz:
-    def affiche(self,n=100):
+    def affiche(self,n:int):
         res=""
-        for i in range(1, n+1):
-            for i in range(1, 101):
-                if i % 15 == 0:
+        for i in range(1,n+1):
+            if i % 15 == 0:
                     res += "FrisBee"
-                elif i % 5 == 0:
+            elif i % 5 == 0:
                     res += "Buzz"
-                elif i % 3 == 0:
+            elif i % 3 == 0:
                     res += "Fizz"
-                else:
+            else:
                     res += str(i)
         return res
