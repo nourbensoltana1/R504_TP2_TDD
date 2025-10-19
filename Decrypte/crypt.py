7034d40e3578c1f2eb0e9c90ec6d955a28bd69f4
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-19 10:36:10 +0200

    Code 4 SUCCESS – crypt(message, pas) décale chaque caractère selon le pas et ajoute le pas à la fin du message

--- Decrypte/crypt.py
+++ Decrypte/crypt.py
@@ -1,11 +1,11 @@
 import string
-def crypt(message):
+def crypt(message,pas):
     caracteres = string.ascii_letters + string.punctuation + string.digits + " "
     result=""
     for i in message:
         position=caracteres.find(i)
-        if position!=len(caracteres)-1:
-            result+=caracteres[position+1]
+        if position<len(caracteres)-1:
+            result+=caracteres[position+pas]
         else:
             result+='a'
-    return(result)
+    return(result+str(pas))
